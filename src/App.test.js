61c53bebc8f01a0e99337db1e0491e32b7f1f427
Husni Munaya <husnimun@gmail.com>
2019-05-15 21:01:13 +0700

    Mock intersectionObserver in component test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App component testing', () => {
+  beforeEach(() => {
+    window.IntersectionObserver = jest.fn()
+    window.IntersectionObserver.prototype.observe = jest.fn()
+    window.IntersectionObserver.prototype.disconnect = jest.fn()
+  })
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+})
